57370afa4b3e48c41b322e801eefe0c068817d68
Cai Lu <caillu@uw.edu>
2016-12-30 09:24:38 -0800

    Add server test, allow shadowing of response

--- tests/server.js
+++ tests/server.js
@@ -1,46 +1,56 @@
 const mysql = require('mysql');
 const request = require('request');
 const expect = require('chai').expect;
+const assert = require('chai').assert;
+const baseUrl = 'http://127.0.0.1:8000';
 
-describe('Testing functionalty', () => {
+describe('Server and database', () => {
   let dbConnection;
+
   beforeEach((done) => {
     dbConnection = mysql.createConnection({
       user: 'root',
       password: '',
       database: 'scribeDB'
     });
-    dbConnection.connect();
 
+    dbConnection.connect();
     const tablename = 'truncate messages';
 
     // Empty the db table before each test so that multiple tests won't screw each other up
     dbConnection.query(tablename, done);
   });
 
-  it('Posting A Message', (done) => {
-    request({
-      method: 'POST',
-      url: 'http://127.0.0.1:8000/messages',
-      json: { text: 'A testing message' }
-    }, (err, results) => {
-      expect(results.statusCode).to.equal(201);
+  it('should return a status code of 200', (done) => {
+    request.get(`${baseUrl}/messages`, (err, response) => {
+      assert.equal(200, response.statusCode);
       done();
     });
   });
 
-  it('Getting A Posted Message', (done) => {
+  it('should post a message to the database', (done) => {
     request({
       method: 'POST',
-      url: 'http://127.0.0.1:8000/messages',
+      url: `${baseUrl}/messages`,
       json: { text: 'A testing message' }
-    }, (err1, results1) => {
-      expect(results1.statusCode).to.equal(201);
+    }, (err, response) => {
+      expect(response.statusCode).to.equal(201);
+      done();
+    });
+  });
+
+  it('should get a message from the database', (done) => {
+    request({
+      method: 'POST',
+      url: `${baseUrl}/messages`,
+      json: { text: 'A testing message' }
+    }, (err1, response) => {
+      expect(response.statusCode).to.equal(201);
       request({
         method: 'GET',
-        url: 'http://127.0.0.1:8000/messages'
-      }, (err2, results2) => {
-        expect(JSON.parse(results2.body)[0].text).to.equal('A testing message');
+        url: `${baseUrl}/messages`
+      }, (err2, response) => {
+        expect(JSON.parse(response.body)[0].text).to.equal('A testing message');
         done();
       });
     });
