8e172c2178ffb31934bd096dc67e265d115eec34
d-burke <omnidan@yahoo.com>
2017-01-02 20:19:08 -0800

    server and db location functionality

--- controllers/index.js
+++ controllers/index.js
@@ -3,21 +3,47 @@ const db = require('../db');
 module.exports = {
   messages: {
     get: function (req, res) {
-      console.log('get req: ', req.query);
-      db.Message.findAll({})
-      .then((data) => {
-        res.json(data);
-      })
-      .catch((error) => {
-        console.log('error: ', error);
-      });
+      // console.log('get req: ', req.query);
+      if (req.query.latitude && req.query.longitude) { // whereas here has query
+        const latitude = parseFloat(req.query.latitude);
+        const longitude = parseFloat(req.query.longitude);
+        db.Message.findAll({
+          where: {
+            latitude: {
+              $between: [latitude - 1, latitude + 1]
+            },
+            longitude: {
+              $between: [longitude - 1, longitude + 1]
+            }
+          }
+        })
+        .then((data) => {
+          console.log('data: ', data);
+          res.json(data);
+        })
+        .catch((error) => {
+          console.log('error: ', error);
+        });
+      } else {
+        db.Message.findAll({}) // find all with no query.
+        .then((data) => {
+          console.log('data: ', data);
+          res.json(data);
+        })
+        .catch((error) => {
+          console.log('error: ', error);
+        });
+      }
     },
     post: function (req, res) {
+      // console.log('post req: ', req.body);
       if (req.body.text.length < 1) {
         res.sendStatus(406);
       } else {
         db.Message.create({
-          text: req.body.text
+          text: req.body.text,
+          latitude: req.body.latitude,
+          longitude: req.body.longitude
         })
         .then(() => {
           res.sendStatus(201);
