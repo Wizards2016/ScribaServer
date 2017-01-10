7a1d4885b5bf33e7fa2f093468560987119730db
Cai Lu <caillu@uw.edu>
2017-01-10 10:38:47 -0800

    (tests) Add messages tests

--- tests/server.js
+++ tests/server.js
@@ -1,130 +1,285 @@
-const mysql = require('mysql');
-const request = require('request');
 const expect = require('chai').expect;
 const assert = require('chai').assert;
-
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('scribedb', 'root', '');
+const Models = require('../db');
+const request = require('request');
+const server = require('../server');
 const baseUrl = 'http://127.0.0.1:8000';
-// Start the server
-require('../server.js');
 
-describe('Server and database', () => {
-  let dbConnection;
+xdescribe('Server', () => {
+  it('Send a status of 200 when requesting to /messages', (done) => {
+    request({
+      method: 'GET',
+      url: `${baseUrl}/messages`
+    }, (error, response) => {
+      expect(response.statusCode).to.equal(200);
+    });
+    done();
+  });
+
+  xit('Send a status of 200 when requesting to /users', (done) => {
+    request({
+      method: 'GET',
+      url: `${baseUrl}/users`
+    }, (error, response) => {
+      expect(response.statusCode).to.equal(200);
+    });
+    done();
+  });
+
+  xit('Send a status of 200 when requesting to /votes', (done) => {
+    request({
+      method: 'GET',
+      url: `${baseUrl}/votes`
+    }, (error, response) => {
+      expect(response.statusCode).to.equal(200);
+    });
+    done();
+  });
+});
+
+describe('API & Database', () => {
 
   beforeEach((done) => {
-    // If running in the terminal:
-    // start mysql server and use scribedb database
-    dbConnection = mysql.createConnection({
-      user: 'root',
-      password: '',
-      database: 'scribedb'
-    });
-
-    dbConnection.connect();
-    const tablename = 'truncate Messages';
-
-    // Empty the db table before each test so that multiple tests won't screw each other up
-    dbConnection.query(tablename, done);
-  });
-
-  it('should return a status code of 200', (done) => {
-    request.get(`${baseUrl}/messages`, (err, response) => {
-      assert.equal(200, response.statusCode);
+    Models.Messages.destroy({
+      where: {}
+    })
+    .then(() => {
+      Models.Users.destroy({
+        where: {}
+      });
+    })
+    .then(() => {
+      Models.Votes.destroy({
+        where: {}
+      });
+    })
+    .then(() => {
       done();
     });
   });
 
-  it('should post a message to the database', (done) => {
-    request({
-      method: 'POST',
-      url: `${baseUrl}/messages`,
-      json: { text: 'test #1' }
-    }, (err, response) => {
-      expect(response.statusCode).to.equal(201);
+  xdescribe('MESSAGES', () => {
+    const messagesURL = `${baseUrl}/messages`;
+
+    const messages = [{
+      text: 'Yard Sale!',
+      latitude: 37.3323314,
+      longitude: -122.0342186,
+      userAuth: 'Thomas Cruise',
+      displayName: 'ThomasCruise'
+    }, {
+      text: 'Free couch',
+      latitude: 39.5344314,
+      longitude: -122.0336186,
+      userAuth: 'Thomas Cruise',
+      displayName: 'ThomasCruise'
+    }];
+
+    const position = {
+      latitude: 37.3323314,
+      longitude: -122.0342186
+    };
+
+
+    describe('GET', () => {
+      beforeEach((done) => {
+        // Empty the Messages table
+        Models.Messages.destroy({
+          where: {}
+        })
+        .then(() => {
+          done();
+        });
+      });
+
+      it('Get all messages if latitude and longitude are not specified', (done) => {
+        messages.forEach((message, index) => {
+          Models.Messages.create(message)
+            .then(() => {
+              if (index === messages.length - 1) {
+                request({
+                  method: 'GET',
+                  url: messagesURL
+                }, (error, response, body) => {
+                  expect(JSON.parse(body).length).to.equal(2);
+                  done();
+                });
+              }
+            });
+        });
+      });
+
+      // TODO: How to send data along with a get request?
+      xit('Get nearby messages if latitude and longitude are specified', (done) => {
+        request({
+          method: 'GET',
+          url: messagesURL,
+          json: {
+            latitude: 37,
+            longitude: -122.0342186
+          }
+        }, (error, response, body) => {
+          // console.log('========================================', response);
+          expect(JSON.parse(body).length).to.equal(1);
+          done();
+        });
+      });
     });
-    request({
-      method: 'POST',
-      url: `${baseUrl}/messages`,
-      json: { text: 'test #2' }
-    }, (err, response) => {
-      expect(response.statusCode).to.equal(201);
-      done();
+
+    describe('POST', () => {
+      beforeEach((done) => {
+        Models.Users.destroy({
+          where: {}
+        })
+        .then(() => {
+          Models.Messages.destroy({
+            where: {}
+          });
+        })
+        .then(() => {
+          Models.Users.create({
+            displayName: 'Jean Valjean',
+            userAuth: '1234567890'
+          });
+        })
+        .then(() => {
+          done();
+        });
+      });
+
+      it('Post a message', (done) => {
+
+        request({
+          method: 'POST',
+          url: messagesURL,
+          json: {
+            text: 'Hello world',
+            latitude: 39.5344314,
+            longitude: 122.0336186,
+            displayName: 'Jean Valjean',
+            userAuth: '1234567890'
+          }
+        }, (error, response, body) => {
+          expect(response.statusCode).to.equal(201);
+          done();
+        });
+      });
+
+      it('Delete a message', (done) => {
+        // Post a message with this user
+        request({
+          method: 'POST',
+          url: messagesURL,
+          json: {
+            text: 'Hello world',
+            latitude: 39.5344314,
+            longitude: 122.0336186,
+            displayName: 'Jean Valjean',
+            userAuth: '1234567890'
+          }
+        }, (error, response, body) => {
+          // Get the id of the message that was posted
+          request({
+            method: 'GET',
+            url: messagesURL
+          }, (error, response, body) => {
+            const messageID = JSON.parse(body)[0].id;
+            // Send a delete request for this message
+            request({
+              method: 'POST',
+              url: messagesURL,
+              json: {
+                delete: true,
+                id: messageID,
+                displayName: 'Jean Valjean',
+                userAuth: '1234567890'
+              }
+            }, (error, response, body) => {
+              // Check that the message was deleted
+              request({
+                method: 'GET',
+                url: messagesURL
+              }, (error, response, body) => {
+                expect(JSON.parse(body).length).to.equal(0);
+                done();
+              });
+            });
+          });
+        });
+      });
+
+      it('Should not post a message if text length is less than 1', (done) => {
+        // Post a message with this user
+        request({
+          method: 'POST',
+          url: messagesURL,
+          json: {
+            text: '',
+            latitude: 39.5344314,
+            longitude: 122.0336186,
+            displayName: 'Jean Valjean',
+            userAuth: '1234567890'
+          }
+        }, (error, response, body) => {
+          // Check that the message was not posted
+          request({
+            method: 'GET',
+            url: messagesURL
+          }, (error, response, body) => {
+            expect(JSON.parse(body).length).to.equal(0);
+            done();
+          });
+        });
+      });
+
+      it('Should not post a message if location is not given', (done) => {
+        // Post a message with this user
+        request({
+          method: 'POST',
+          url: messagesURL,
+          json: {
+            text: 'Hello world',
+            displayName: 'Jean Valjean',
+            userAuth: '1234567890'
+          }
+        }, (error, response, body) => {
+          // Check that the message was not posted
+          request({
+            method: 'GET',
+            url: messagesURL
+          }, (error, response, body) => {
+            expect(JSON.parse(body).length).to.equal(0);
+            done();
+          });
+        });
+      });
+
+      it('Should respond with 404 when an invalid displayName or userAuth is given', (done) => {
+        // Post a message with an incorrect user
+        request({
+          method: 'POST',
+          url: messagesURL,
+          json: {
+            text: 'Hello world',
+            latitude: 39.5344314,
+            longitude: 122.0336186,
+            displayName: 'Jean Valjohn',
+            userAuth: '1234567890'
+          }
+        }, (error, response, body) => {
+          // Check that the message was not posted
+          expect(response.statusCode).to.equal(400);
+          done();
+        });
+      });
     });
   });
 
-  it('should not post an empty message to the database', (done) => {
-    request({
-      method: 'POST',
-      url: `${baseUrl}/messages`,
-      json: { text: '' }
-    }, (err, response) => {
-      expect(response.statusCode).to.equal(406);
-      done();
-    });
-  });
+  describe('USERS', () => {
+    beforeEach((done) => {
 
-  it('should get a message from the database', (done) => {
-    request({
-      method: 'POST',
-      url: `${baseUrl}/messages`,
-      json: { text: 'Test #1' }
-    }, (err1, response) => {
-      expect(response.statusCode).to.equal(201);
-      request({
-        method: 'GET',
-        url: `${baseUrl}/messages`
-      }, (err2, response) => {
-        expect(JSON.parse(response.body)[0].text).to.equal('Test #1');
-      });
-    });
-
-    request({
-      method: 'POST',
-      url: `${baseUrl}/messages`,
-      json: { text: 'Test #2' }
-    }, (err1, response) => {
-      expect(response.statusCode).to.equal(201);
-      request({
-        method: 'GET',
-        url: `${baseUrl}/messages`
-      }, (err2, response) => {
-        expect(JSON.parse(response.body)[1].text).to.equal('Test #2');
-        done();
-      });
-    });
-  });
-
-  it('should get a specific message from the database', (done) => {
-    request({
-      method: 'POST',
-      url: `${baseUrl}/messages`,
-      json: { text: 'Test #1', latitude: '37', longitude: '-100' }
-    }, (err1, response) => {
-      expect(response.statusCode).to.equal(201);
-      request({
-        method: 'GET',
-        url: `${baseUrl}/messages`,
-        json: { latitude: '37', longitude: '-100' }
-      }, (err2, response) => {
-        expect(response.body[0].latitude).to.equal(37);
-        expect(response.body[0].longitude).to.equal(-100);
-      });
-    });
-
-    request({
-      method: 'POST',
-      url: `${baseUrl}/messages`,
-      json: { text: 'Test #1', latitude: '40.7128', longitude: '74.0059' }
-    }, (err1, response) => {
-      expect(response.statusCode).to.equal(201);
-      request({
-        method: 'GET',
-        url: `${baseUrl}/messages`,
-        json: { latitude: '40.7128', longitude: '74.0059' }
-      }, (err2, response) => {
-        expect(response.body[1].latitude).to.equal(40.7128);
-        expect(response.body[1].longitude).to.equal(74.0059);
-        done();
-      });
     });
   });
 });
